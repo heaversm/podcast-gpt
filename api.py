2359393639bf36ad8be2a024b32293463919e1db
Mike Heavers <mheavers@gmail.com>
2023-03-13 12:31:51 -0700

    turn into a function call that accepts params

--- api.py
+++ api.py
@@ -1,18 +1,26 @@
 import os
+import sys
 import dotenv
 import openai
 
 config = dotenv.dotenv_values(".env")
 openai.api_key = config['OPENAI_API_KEY']
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="Write a tagline for an ice cream shop",
-  temperature=0.7,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
+# COMMAND LINE USAGE:
+# python api.py generate_from_prompt "This is a prompt"
 
-print(response.choices[0].text)
+def generate_from_prompt(promptString):
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=promptString,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+
+  print(response.choices[0].text)
+
+if __name__ == '__main__':
+    globals()[sys.argv[1]](sys.argv[2])
